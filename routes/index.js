13c38f1cef0753e322e79b43456ab844847d043c
zixizhao <981282392@qq.com>
2022-12-04 15:30:58 -0700

    Done.

--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.post('/edit', (req, res, next) => {
       exit(1);
     }
     console.log("Updating post: " + req.body.blog  + " New Content: " + req.body.content);
-    db.exec(`update posts set blog_txt = '${req.body.txt}' where blog_id=${req.body.blog}`)
+    db.exec(`update blog set blog_txt = '${req.body.txt}' where blog_id=${req.body.blog}`)
     //redirect to homepage
     res.redirect('/');
   }
